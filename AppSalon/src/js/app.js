e61da8512ff39ee28fe3440ca0b18ae612dceb44
julicastro <castroejulian@gmail.com>
2021-04-05 04:29:16 -0300

    App Salon: Cambiar Seccion

---
+++ AppSalon/src/js/app.js
@@ -0,0 +1,160 @@
+let pagina = 1;
+
+document.addEventListener('DOMContentLoaded', function () {
+    iniciarApp();
+
+});
+
+function iniciarApp() {
+    mostrarServicios();
+    // resalta div actual segun tag presionado
+    mostrarSeccion();
+    // oculta o muestra seccion segun tag presionado
+    cambiarSeccion();
+    // paginacion siguinete y anterior
+    paginaSiguiente();
+    paginaAnterior();
+    // comprueba pagina actual para ocultar o mostrar
+    botonesPaginador();
+}
+
+
+function mostrarSeccion() {
+
+    // Eliminar mostrar-seccion de la sección anterior
+    const seccionAnterior = document.querySelector('.mostrar-seccion');
+    if( seccionAnterior ) {
+        seccionAnterior.classList.remove('mostrar-seccion');
+    }
+
+    const seccionActual = document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+
+    // Eliminar la clase de actual en el tab anterior
+    const tabAnterior = document.querySelector('.tabs .actual');
+    if(tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+   
+    // Resalta el Tab Actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
+}
+
+function cambiarSeccion() {
+    const enlaces = document.querySelectorAll('.tabs button');
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+            pagina = parseInt(e.target.dataset.paso);
+
+            // Llamar la función de mostrar sección
+            mostrarSeccion();
+            botonesPaginador();
+        })
+    })
+}
+
+
+async function mostrarServicios() {
+    try {
+        const resultado = await fetch('./servicios.json');
+        const db = await resultado.json();
+
+        const { servicios } = db;
+
+        // Generar el HTML
+        servicios.forEach(servicio => {
+            const { id, nombre, precio } = servicio;
+
+            // DOM Scripting
+            // Generar nombre de servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+
+            // Generar el precio del servicio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            // Generar div contenedor de servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+            servicioDiv.dataset.idServicio = id;
+
+            // Selecciona un servicio para la cita
+            servicioDiv.onclick = seleccionarServicio;
+
+
+            // Inyectar precio y nombre al div de servicio
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // Inyectarlo en el HTML
+            document.querySelector('#servicios').appendChild(servicioDiv);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function seleccionarServicio(e) {
+
+    let elemento;
+    // Forzar que el  elemento al cual le damos click sea el DIV 
+    if (e.target.tagName === 'P') {
+        elemento = e.target.parentElement;
+    } else {
+        elemento = e.target;
+    }
+
+    if (elemento.classList.contains('seleccionado')) {
+        elemento.classList.remove('seleccionado');
+
+        const id = parseInt(elemento.dataset.idServicio);
+
+        eliminarServicio(id);
+    } else {
+        elemento.classList.add('seleccionado');
+
+    }
+
+}
+
+function paginaSiguiente(){
+    const paginaSiguiente = document.querySelector("#siguiente");
+    paginaSiguiente.addEventListener("click", () =>{
+        pagina++;
+
+        botonesPaginador();
+    });
+}
+
+function paginaAnterior (){
+    const paginaAnterior = document.querySelector("#anterior");
+    paginaAnterior.addEventListener("click", () =>{
+        pagina--;
+
+        botonesPaginador();
+
+    });
+}
+function botonesPaginador() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    const paginaAnterior = document.querySelector('#anterior');
+
+    if(pagina  === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (pagina === 3) {
+        paginaSiguiente.classList.add('ocultar');
+        paginaAnterior.classList.remove('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    }
+
+    mostrarSeccion(); // Cambia la sección que se muestra por la de la página
+}
