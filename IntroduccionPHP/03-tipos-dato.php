b3cc8be03475b0eb4c0f8a2e904915cd53a84452
julicastro <castroejulian@gmail.com>
2021-04-13 01:20:37 -0300

    php intro

---
+++ IntroduccionPHP/03-tipos-dato.php
@@ -0,0 +1,28 @@
+<?php include 'includes/header.php';
+
+// Boolean
+$logueado = false;
+var_dump($logueado);
+
+// Entero
+$numero = 200;
+var_dump($numero);
+
+//Floats
+$float = 200.5;
+var_dump($float);
+
+// String
+$nombre = "Juli";
+var_dump($nombre);
+
+//Arreglo
+$array = [];
+var_dump($array);
+
+
+
+
+
+
+include 'includes/footer.php';
