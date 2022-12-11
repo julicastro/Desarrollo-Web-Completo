ae21880304e0dcda08fa9a5b140c540cf433733b
julicastro <castroejulian@gmail.com>
2022-12-11 20:22:42 -0300

    gulp

--- Sass-Gulp/Gulp/gulpfile.js
+++ Sass-Gulp/Gulp/gulpfile.js
@@ -12,19 +12,26 @@ function tarea() {
 /* export. "nombre con el q lo llamo" */
 exports.tarea = tarea;
 
-const { src, dest, watch } = require('gulp');
-const sass = require("gulp-sass")(require('sass'));
-const plumber = require("gulp-plumber");
-
+const { src, dest, watch, parallel } = require('gulp');
+/*
+    src -> identifica directorios
+    dest -> permite almacenar en carpeta destino
+*/
 /*
  el gulp este es el del package.json
 y el require es una forma de extraerlo 
 extraigo funciones q incluye gulp 
 */
-/*
-    src -> identifica directorios
-    dest -> permite almacenar en carpeta destino
-*/
+
+// CSS
+const sass = require("gulp-sass")(require('sass'));
+const plumber = require("gulp-plumber");
+
+// Imagenes
+const cache = require('gulp-cache');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+
 
 function css(done) {
 
@@ -37,14 +44,37 @@ function css(done) {
     done();
 }
 
+function versionWebp(done) {
+    const opciones = {
+        quality: 50 // 0 a 100
+    };
+    src('src/img/**/*.{png, jpg}')
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'))
+        done();
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel: 3
+    };
+    src('src/img/**/*.{png, jpg}')
+    .pipe( cache(imagemin(opciones)) )
+    .pipe(dest('build/img'))
+
+    done();
+}
+
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
     /* mantiene abierta la funcion */
     done();
 }
 
-
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
 
 
